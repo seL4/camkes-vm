df8f2d986c20bcd87f4df190be1ff01d29b45747
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-02-18 11:05:57 +1100

    Add some description to the various parts of the VM configuration. Placing this inline in c162_threevm_testing as it is the most complicated configuration

--- apps/vm/configurations/c162_threevm_testing.h
+++ apps/vm/configurations/c162_threevm_testing.h
@@ -8,8 +8,41 @@
  * @TAG(NICTA_GPL)
  */
 
-/* We expect 3 VMs */
+/* This entire configuration file is a bit of a mess. There is various duplication
+ * of information, not to mention the fact that this is all done in macros.
+ * Ideally most of the logic for the VMs configuration could be moved to
+ * a template and camkes configurations could be used along with some slightly
+ * nicer code generation.
+ *
+ * When modifying this it is important to remember that some macros just expand to
+ * their contents, and some macros are expected to be used in boost list expansions
+ * and care must be taken to get the syntax correct else difficult to debug build
+ * errors will occur. For a boost list an empty list would be defined here as
+   #define EMPTY_LIST()
+ * Where as a list with one element is
+   #define ONE_ELEMENT() \
+        element \
+   )
+ * And multiple elements becomes
+   #define MULTI_ELEMENT() \
+        element1, \
+        element2, \
+        element3
+   )
+ * Importantly note that you cannot have a trailing comma on the last element
+ * of a list
+ *
+ * Many options have _N varients. The N here refers to which VM number the option
+ * applies to. This is the mechanism by which different VMs can be given different
+ * resources */
 
+
+/* If VM_CONFIGURATION_EXTRA_RAM is defined then the corresponding _N definitions
+ * will be used to find ranges for extra ram. These are boost lists and contain
+ * physical address and 2^n size pairs. Each range must fall within a single
+ * untyped that is given to the rootserver (capdl-loader) by the kernel. The capdl-loader
+ * has a configuration option that can be turned on that will print out what
+ * device untypeds it has received */
 #define VM_CONFIGURATION_EXTRA_RAM
 #define VM_CONFIGURATION_EXTRA_RAM_0() ( \
         (0x21000000,24), \
@@ -30,6 +63,21 @@
     ) \
     /**/
 
+/* Passthrough IRQs are used to give a guest a direct hardware interrupt. The format here
+ * is (source_irq, level_triggered, active_low, dest_irq).
+ * source_irq - The interrupt index on the I/O APIC for this device. Note that seL4
+ *              does not interrupt remapping of any kind and will just use the default
+ * level_triggered - 1 if level triggred 0 if edge triggered. Generally PCI devices are
+ *                   level triggered and old ISA devices (serial ports etc) are edge
+ *                   triggered
+ * active_low - 1 if triggered when low, 0 if triggered when high. Generally
+ *              PCI interrupts are active low, and ISA interrupts are active high
+ * dest_irq - The IRQ number on the PIC we emulate to Linux to deliver this interrupt
+ *            to. Frequently the source and dest irqs will need to be different,
+ *            in this case the VMM supports a way to indicate in the PCI config space
+ *            virtualization where the interrupt is. See the comments on
+ *            VM_GUEST_PASSTHROUGH_DEVICES
+ */
 #define VM_PASSTHROUGH_IRQ_0() ( \
         (11, 1, 1, 10), \
         (23, 1, 1, 14) \
@@ -47,6 +95,10 @@
     ) \
     /**/
 
+/* Camkes definitions for defining any connections that are specific
+ * to this VM configuration. In this case we are defining connections
+ * for UDPServer as well as inter-vm communication
+ */
 #define PLAT_CONNECT_DEF() \
     /* Give ethernet driver same output as its vm */ \
     connection seL4RPCCall eth_putchar(from ethdriver0.putchar, to serial.vm1); \
@@ -74,6 +126,13 @@
     connection seL4AsynchBind vm2_net_ready(from vm2.vm0net_emit, to vm0.intready); \
     /**/
 
+/* Define any IOSpaces that need be created and populated with mappings
+ * in the IOMMU. Each entry here is the format
+   "iospace_domain:pci_bus:pci_device:pci_fun"
+ * The iospace domain needs to match the definition in
+ * VM_GUEST_IOSPACE_DOMAIN_N There needs to be a definition here for
+ * every PCI device given to the guest in VM_GUEST_PASSTHROUGH_DEVICES
+ */
 #define VM_CONFIGURATION_IOSPACES_0() ( \
     /* Sata controller */ \
     "0xf:0x0:0x1f:2", \
@@ -96,6 +155,21 @@
     ) \
     /**/
 
+/* This is a list of any memory mapped IO regions that will be needed when
+ * giving the guest PCI devices. When the VMM gives the guest access to
+ * devices in VM_GUEST_PASSTHROUGH_DEVICES, the memory regions for the
+ * bars need to be in this list. Essentially this list requests the
+ * capdl-loader give the VMM these resources, which we then may or may
+ * not actually give to Linux, depending on whether a device that Linux
+ * uses actually needs it. Format is
+   "physical_address:size:page_bits"
+ * Size can be less than a page, although in practice the actual region
+ * requested will be round up to a multiple of the page size.
+ * page_bits is size of the frame that backs this region. This is to account
+ * for seL4 potentially giving large frames for device regions that can
+ * support it. In practice this doesn't happen at the moment and this
+ * value should always be 12
+ */
 #define VM_CONFIGURATION_MMIO_0() ( \
     /* VME */ \
     "0xe0000000:0x1000:12", \
@@ -117,7 +191,19 @@
     ) \
     /**/
 
-
+/* Definitions of legacy I/O ports that should be given to each guest.
+ * The format of each entry is
+   (first_port, last_port, not_PCI, description)
+ * the first and last ports are inclusive
+ * not_PCI is an option that exists for legacy reasons and indicates
+ * whether the I/O port will be found and given to the guest from
+ * scanning the bars when giving passthrough devices, or whether it
+ * should just be given to the guest. This means that if 1 is given
+ * and port range is also in a device, then an error will occur as
+ * the VMM will attempt to give the port twice to the guest.
+ * Similarly if 0 is given and the port does not appear in a PCI
+ * device then the range will *not* be given to Linux
+ */
 #define VM_CONFIGURATION_IOPORT_0() \
     /**/
 
@@ -142,6 +228,19 @@
     ) \
     /**/
 
+/* camkes definitions that will get placed in the configuration section
+ * of the camkes assembly. Most of the definitions here are to do with
+ * the udp server and vm to vm communication that is specific to this
+ * configuration. The most important option being set for each VMM is the
+   simple_untypedX_pool = Y
+ * This gives Y untypeds each of size 2^X to th VMM. Multiple definitions
+ * with different X can be given for each VMM. This memory will be used
+ * for almost all allocations (including Linux guest memory) by the VMM.
+ * The exception is that physical frames will be preferentially allocated
+ * from the EXTRA_GUEST_RAM regions instead. Because this configuration
+ * has an EXTRA_GUEST_RAM_REGION we make do with relatively small ammounts
+ * of untyped memory
+ */
 #define PLAT_CONFIG_DEF() \
     vm0.simple_untyped24_pool = 2; \
     vm1.simple_untyped24_pool = 2; \
@@ -161,6 +260,18 @@
     vm2.vm0net_attributes ="06,00,00,20,12,14:13:0x9000"; \
     /**/
 
+/* List of pci devices that should be given as passthrough to the guest
+ * Format is
+   {.ven = pci_vendor_id, .dev = pci_device_id, .fun = pci_function, .irq = irq_remap}
+ * The pci_function is an option argument, and is used as a rudimentary
+ * way of picking when there are multiple of the same device. This assumes
+ * that duplicate devices will be on the same pci bus/device and only
+ * differ by function. A value of -1 means the device can appear at
+ * any function
+ * irq_remap is where we will tell the guest the IRQ is. This should probably
+ * match at least one dest_irq in VM_PASSTHROUGH_IRQ. Can be -1 to use the
+ * default *PIC* IRQ
+ */
 #define VM_GUEST_PASSTHROUGH_DEVICES_0() \
     {.ven = 0x8086, .dev = 0x3b34, .fun = -1, .irq = 14}, /* USB */ \
     {.ven = 0x10e3, .dev = 0x0148, .fun = -1, .irq = 10}, /* VMEBus */ \

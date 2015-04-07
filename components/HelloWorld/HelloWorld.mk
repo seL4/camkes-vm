d01884236854e3f357eaa5657e0ebcd8a3334706
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-04-07 17:15:59 +1000

    Make HelloWorld and UDPServer global components

---
+++ components/HelloWorld/HelloWorld.mk
@@ -0,0 +1,7 @@
+CURRENT_DIR := $(dir $(abspath $(lastword ${MAKEFILE_LIST})))
+
+HelloWorld_CFILES := $(wildcard ${CURRENT_DIR}/src/*.c)
+HelloWorld_HFILES := $(wildcard $(CURRENT_DIR)/../../apps/vm/configurations/*.h)
+HelloWorld_HFILES += $(wildcard $(CURRENT_DIR)/include/*.h)
+HelloWorld_LIBS := sel4camkes sel4vmm sel4utils sel4vka sel4allocman sel4vspace sel4simple sel4simple-stable
+
---
+++ components/UDPServer/UDPServer.mk
@@ -0,0 +1,5 @@
+CURRENT_DIR := $(dir $(abspath $(lastword ${MAKEFILE_LIST})))
+
+UDPServer_CFILES := $(wildcard ${CURRENT_DIR}/src/*.c)
+UDPServer_LIBS := sel4camkes ethdrivers lwip
+

35c64db77fb4cf550dfb31e31123f89d1a90fb3e
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-04-07 17:34:46 +1000

    Make Vchan a global component

---
+++ components/Vchan/Vchan.mk
@@ -0,0 +1,6 @@
+CURRENT_DIR := $(dir $(abspath $(lastword ${MAKEFILE_LIST})))
+
+Vchan_CFILES := $(wildcard ${CURRENT_DIR}/src/*.c)
+Vchan_HFILES := $(wildcard $(CURRENT_DIR)/../../apps/vm/configurations/*.h)
+Vchan_LIBS := sel4camkes sel4vmm sel4utils sel4vka sel4allocman sel4vspace sel4simple sel4simple-stable sel4platsupport
+

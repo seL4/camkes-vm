7c62580a86634757c16f6aaadde60768f424911b
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-04-17 11:11:52 +1000

    Use full hex values on attributes

---
+++ templates/seL4ExtraRAM.template.c
@@ -0,0 +1,20 @@
+/*#
+ *# Copyright 2014, NICTA
+ *#
+ *# This software may be distributed and modified according to the terms of
+ *# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ *# See "LICENSE_BSD2.txt" for details.
+ *#
+ *# @TAG(NICTA_BSD)
+ #*/
+
+#include <assert.h>
+#include <camkes/error.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <sel4/sel4.h>
+
+/*? macros.show_includes(me.from_instance.type.includes) ?*/
+
+/*- set instance = me.to_instance.name -*/
+/*- set interface = me.to_interface.name -*/

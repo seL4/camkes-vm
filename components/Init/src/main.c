35321a50e7af699e8dfd699b8baebe39f1e4c692
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-05-05 16:43:07 +1000

    Do not explicity call vchan_interrupt, it will be called by the device notify list

--- components/Init/src/main.c
+++ components/Init/src/main.c
@@ -532,9 +532,6 @@ static int handle_async_event(seL4_Word badge) {
         if ( (badge & VM_PIC_BADGE_SERIAL_HAS_DATA) == VM_PIC_BADGE_SERIAL_HAS_DATA) {
             serial_character_interrupt();
         }
-        if ( (badge & VM_PIC_BADGE_VCHAN_HAS_DATA) == VM_PIC_BADGE_VCHAN_HAS_DATA) {
-            vchan_interrupt(&vmm);
-        }
         for (int i = 0; i < 16; i++) {
             if ( (badge & irq_badges[i]) == irq_badges[i]) {
                 i8259_gen_irq(i);

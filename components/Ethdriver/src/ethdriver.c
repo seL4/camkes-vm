a99d738e72b7d26cb5945901070ff6f7081c2b5c
Damon Lee <Damon.Lee@data61.csiro.au>
2019-03-13 15:15:29 +1100

    Ethdriver: Refactor ethdriver.c to use new platform specific code

--- components/Ethdriver/src/ethdriver.c
+++ components/Ethdriver/src/ethdriver.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, Data61
+ * Copyright 2019, Data61
  * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
  * ABN 41 687 119 230.
  *
@@ -23,10 +23,11 @@
 #include <allocman/bootstrap.h>
 #include <allocman/vka.h>
 #include <sel4utils/vspace.h>
-#include <ethdrivers/intel.h>
 #include <ethdrivers/raw.h>
-#include <sel4utils/iommu_dma.h>
-#include <sel4platsupport/arch/io.h>
+#include <ethdrivers/intel.h>
+#include <sel4utils/sel4_zf_logif.h>
+
+#include "plat.h"
 
 #define RX_BUFS 256
 
@@ -48,7 +49,6 @@ static vka_t vka;
 static vspace_t vspace;
 static sel4utils_alloc_data_t vspace_data;
 static struct eth_driver eth_driver;
-static seL4_CPtr (*original_vspace_get_cap)(vspace_t*, void*);
 
 void camkes_make_simple(simple_t *simple);
 
@@ -398,77 +398,27 @@ void irq_handle() {
     error = ethdriver_unlock();
 }
 
-/* Returns the cap to the frame mapped to vaddr, assuming
- * vaddr points inside our dma pool. */
-static seL4_CPtr get_dma_frame_cap(vspace_t *vspace, void *vaddr) {
-    seL4_CPtr cap = camkes_dma_get_cptr(vaddr);
-    if (cap == seL4_CapNull) {
-        return original_vspace_get_cap(vspace, vaddr);
-    }
-    return cap;
-}
-
-/* Allocate a dma buffer backed by the component's dma pool */
-static void* camkes_iommu_dma_alloc(void *cookie, size_t size,
-        int align, int cached, ps_mem_flags_t flags) {
-
-    // allocate buffer from the dma pool
-    void* vaddr = camkes_dma_alloc(size, align);
-    if (vaddr == NULL) {
-        return NULL;
-    }
-    int error = sel4utils_iommu_dma_alloc_iospace(cookie, vaddr, size);
-    if (error) {
-        camkes_dma_free(vaddr, size);
-        return NULL;
-    }
-    return vaddr;
-}
-
-/* depending on the actual component a different init function will be linked against */
-int ethif_init(struct eth_driver *eth_driver, ps_io_ops_t io_ops, void *config);
-
 void post_init(void) {
-    int error;
-    int pci_bdf_int;
-    int bus, dev, fun;
+    int error = 0;
     error = ethdriver_lock();
     /* initialize seL4 allocators and give us a half sane environment */
     init_system();
-    /* initialize the driver */
-    ps_io_ops_t ioops;
-    cspacepath_t iospace;
-    error = vka_cspace_alloc_path(&vka, &iospace);
-    assert(!error);
-    sscanf(pci_bdf, "%x:%x.%d", &bus, &dev, &fun);
-    pci_bdf_int = bus * 256 + dev * 8 + fun;
-    /* get this from the configuration */
-    error = simple_get_iospace(&camkes_simple, iospace_id, pci_bdf_int, &iospace);
-    assert(!error);
 
-    /* Save a pointer to the original get_cap function for our vspace */
-    original_vspace_get_cap = vspace.get_cap;
+    ps_io_ops_t io_ops = {0};
 
-    /* The iommu driver needs the caps to frames backing the dma buffer.
-     * It will invoke the get_cap method of its vspace to get these caps.
-     * Since the vspace we give to the iommu driver wasn't used to allocate
-     * the dma buffer, it doesn't know the caps to the frames backing the
-     * buffer. CAmkES allocated the buffer statically, and so the caps are
-     * known to it. Here, we override the get_cap method of our vspace to
-     * return dma buffer frame caps provided by CAmkES. */
-    vspace.get_cap = get_dma_frame_cap;
+    eth_driver.cb_cookie = NULL;
+    eth_driver.i_cb = ethdriver_callbacks;
 
-    error = sel4utils_make_iommu_dma_alloc(&vka, &vspace, &ioops.dma_manager, 1, &iospace.capPtr);
-    assert(!error);
-    ioops.dma_manager.dma_alloc_fn = camkes_iommu_dma_alloc;
+    /* initialise the driver */
+    error = ethif_preinit(&vka, &camkes_simple, &vspace, &io_ops);
+    ZF_LOGF_IF(error, "Failed to setup the initialisation of the ethernet interface");
 
-    error = sel4platsupport_get_io_port_ops(&ioops.io_port_ops, &camkes_simple, &vka);
-    assert(!error);
     /* preallocate buffers */
     for (int i = 0; i < RX_BUFS; i++) {
-        void *buf = ps_dma_alloc(&ioops.dma_manager, BUF_SIZE, 4, 1, PS_MEM_NORMAL);
+        void *buf = ps_dma_alloc(&io_ops.dma_manager, BUF_SIZE, 4, 1, PS_MEM_NORMAL);
         assert(buf);
-        uintptr_t phys = ps_dma_pin(&ioops.dma_manager, buf, BUF_SIZE);
+        memset(buf, 0, BUF_SIZE);
+        uintptr_t phys = ps_dma_pin(&io_ops.dma_manager, buf, BUF_SIZE);
         rx_bufs[num_rx_bufs] = (eth_buf_t) { .buf = buf, .phys = phys };
         rx_buf_pool[num_rx_bufs] = &(rx_bufs[num_rx_bufs]);
         num_rx_bufs++;
@@ -481,9 +431,10 @@ void post_init(void) {
         clients[client].dataport = client_buf(clients[client].client_id);
         client_get_mac(clients[client].client_id, clients[client].mac);
         for (int i = 0; i < CLIENT_TX_BUFS; i++) {
-            void *buf = ps_dma_alloc(&ioops.dma_manager, BUF_SIZE, 4, 1, PS_MEM_NORMAL);
+            void *buf = ps_dma_alloc(&io_ops.dma_manager, BUF_SIZE, 4, 1, PS_MEM_NORMAL);
             assert(buf);
-            uintptr_t phys = ps_dma_pin(&ioops.dma_manager, buf, BUF_SIZE);
+            memset(buf, 0, BUF_SIZE);
+            uintptr_t phys = ps_dma_pin(&io_ops.dma_manager, buf, BUF_SIZE);
             tx_frame_t *tx_buf = &clients[client].tx_mem[clients[client].num_tx];
             *tx_buf = (tx_frame_t){.len = BUF_SIZE, .client = client};
             tx_buf->buf = (eth_buf_t) { .buf = buf, .phys = phys };
@@ -491,15 +442,10 @@ void post_init(void) {
             clients[client].num_tx++;
         }
     }
-    eth_driver.cb_cookie = NULL;
-    eth_driver.i_cb = ethdriver_callbacks;
-    ethif_intel_config_t eth_config = (ethif_intel_config_t) {
-        .bar0 = (void*)EthDriver
-    };
 
-    error = ethif_init(&eth_driver, ioops, &eth_config);
-
-    assert(!error);
+    error = ethif_init(&eth_driver, &io_ops);
+    ZF_LOGF_IF(error, "Failed to initialise the ethernet device");
+    
     done_init = 1;
     error = irq_acknowledge();
     error = ethdriver_unlock();

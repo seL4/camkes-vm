66c30e8673b7317f9556d9f51b5601d626ac45ba
Damon Lee <Damon.Lee@data61.csiro.au>
2019-03-21 13:32:13 -0400

    sataserver: Add sataserver component and support
    This commit combines a number of smaller commits which do the following:
    
        * Add sataserver component and supporting files
        * Convert sataserver to the latest CAmkES release and cmake
        * Fix io ops memory issue in sataserver post init
        * Fixed virtio blk dma alignment to work on both 64 bit and
          32 bit hosts
        * Force virtio blk request to a max of 4096 bytes
        * Update structs to follow changes made in libsatadrivers
        * Adapt Sataserver to AHCI changes in libsatadrivers
        * Add sataserver hardware configuration based on machine
        * Update templates to run without threads
        * Add CHS values to virtual MBR partition entries
        * Fix virtual capacity calculation
        * Lock sataserver sooner on client rx and tx calls
        * Virtualize the MBR
        * Add case for all possible directions
        * Connect to serial server
        * Update hardware addresses for MAC
        * Allow for mapping of multiple iospaces
        * Update allocated objects
        * Indents, line lengths, and WEAK for virtio_blk.c
        * Only have single sataserver badge
        * Remove unused template functions
        * Printf -> ZF_LOGV
        * Add ability to choose drive from CAmkES config
    
    CCDC-GVSC DISTRIBUTION A.  Approved for public release; distribution
    unlimited. OPSEC#4481.
    
    Co-authored-by: Alex Pavey <Alex.Pavey@Dornerworks.com>
    Signed-off-by: Damon Lee <Damon.Lee@data61.csiro.au>

---
+++ components/Sataserver/include/sataserver.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2019, Dornerworks
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+#pragma once
+
+#define SATASERVER_STATUS_GOOD          0
+#define SATASERVER_STATUS_NOT_DONE      1
+#define SATASERVER_STATUS_INVALID_CONF  2

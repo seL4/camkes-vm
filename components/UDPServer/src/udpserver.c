1c5e168d0fd588b74d6abcdf66bb07777519191f
Damon Lee <Damon.Lee@data61.csiro.au>
2020-08-05 12:33:30 +1000

    trivial: Style changed files

--- components/UDPServer/src/udpserver.c
+++ components/UDPServer/src/udpserver.c
@@ -23,24 +23,26 @@
 #include <lwip/igmp.h>
 #include <sel4/sel4.h>
 
-static void low_level_init(struct eth_driver *driver, uint8_t *mac, int *mtu) {
+static void low_level_init(struct eth_driver *driver, uint8_t *mac, int *mtu)
+{
     *mtu = 1500;
-    ethdriver_mac(&mac[0],&mac[1],&mac[2],&mac[3],&mac[4],&mac[5]);
+    ethdriver_mac(&mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]);
 }
 
 extern void *ethdriver_buf;
 
-static void raw_poll(struct eth_driver *driver) {
+static void raw_poll(struct eth_driver *driver)
+{
     int len;
     int status;
     status = ethdriver_rx(&len);
     while (status != -1) {
         void *buf;
         void *cookie;
-        buf = (void*)driver->i_cb.allocate_rx_buf(driver->cb_cookie, len, &cookie);
+        buf = (void *)driver->i_cb.allocate_rx_buf(driver->cb_cookie, len, &cookie);
         assert(buf);
-        memcpy(buf, (void*)ethdriver_buf, len);
-        driver->i_cb.rx_complete(driver->cb_cookie, 1, &cookie, (unsigned int*)&len);
+        memcpy(buf, (void *)ethdriver_buf, len);
+        driver->i_cb.rx_complete(driver->cb_cookie, 1, &cookie, (unsigned int *)&len);
         if (status == 1) {
             status = ethdriver_rx(&len);
         } else {
@@ -51,19 +53,20 @@ static void raw_poll(struct eth_driver *driver) {
     }
 }
 
-static int raw_tx(struct eth_driver *driver, unsigned int num, uintptr_t *phys, unsigned int *len, void *cookie) {
+static int raw_tx(struct eth_driver *driver, unsigned int num, uintptr_t *phys, unsigned int *len, void *cookie)
+{
     unsigned int total_len = 0;
     int i;
-    void *p = (void*)ethdriver_buf;
+    void *p = (void *)ethdriver_buf;
     int err;
 
     for (i = 0; i < num; i++) {
-        memcpy(p + total_len, (void*)phys[i], len[i]);
+        memcpy(p + total_len, (void *)phys[i], len[i]);
         total_len += len[i];
     }
 
     /* Retry whilst the link is down */
-    while((err = ethdriver_tx(total_len)) == ETHIF_TX_FAILED) {
+    while ((err = ethdriver_tx(total_len)) == ETHIF_TX_FAILED) {
         err = lwip_unlock();
         err = lwip_lock();
     }
@@ -71,7 +74,8 @@ static int raw_tx(struct eth_driver *driver, unsigned int num, uintptr_t *phys, 
     return ETHIF_TX_COMPLETE;
 }
 
-static void handle_irq(struct eth_driver *driver, int irq) {
+static void handle_irq(struct eth_driver *driver, int irq)
+{
     raw_poll(driver);
 }
 
@@ -83,14 +87,16 @@ static struct raw_iface_funcs iface_fns = {
     .raw_poll = raw_poll
 };
 
-static int ethdriver_init(struct eth_driver *eth_driver, ps_io_ops_t io_ops, void *config) {
+static int ethdriver_init(struct eth_driver *eth_driver, ps_io_ops_t io_ops, void *config)
+{
     eth_driver->eth_data = NULL;
     eth_driver->dma_alignment = 1;
     eth_driver->i_fn = iface_fns;
     return 0;
 }
 
-static void* malloc_dma_alloc(void *cookie, size_t size, int align, int cached, ps_mem_flags_t flags) {
+static void *malloc_dma_alloc(void *cookie, size_t size, int align, int cached, ps_mem_flags_t flags)
+{
     assert(cached);
     int error;
     void *ret;
@@ -101,25 +107,30 @@ static void* malloc_dma_alloc(void *cookie, size_t size, int align, int cached, 
     return ret;
 }
 
-static void malloc_dma_free(void *cookie, void *addr, size_t size){
+static void malloc_dma_free(void *cookie, void *addr, size_t size)
+{
     free(addr);
 }
 
-static uintptr_t malloc_dma_pin(void *cookie, void *addr, size_t size) {
+static uintptr_t malloc_dma_pin(void *cookie, void *addr, size_t size)
+{
     return (uintptr_t)addr;
 }
 
-static void malloc_dma_unpin(void *cookie, void *addr, size_t size) {
+static void malloc_dma_unpin(void *cookie, void *addr, size_t size)
+{
 }
 
-static void malloc_dma_cache_op(void *cookie, void *addr, size_t size, dma_cache_op_t op) {
+static void malloc_dma_cache_op(void *cookie, void *addr, size_t size, dma_cache_op_t op)
+{
 }
 
 static ps_io_ops_t io_ops;
 static struct netif _netif;
 static lwip_iface_t _lwip_driver;
 
-void pre_init(void) {
+void pre_init(void)
+{
     ip_addr_t netmask, ipaddr, gw, multicast;
     struct netif *netif;
     int UNUSED error;
@@ -144,7 +155,7 @@ void pre_init(void) {
 
     lwip_init();
     netif = netif_add(&_netif, &ipaddr, &netmask, &gw, lwip_driver, ethif_get_ethif_init(lwip_driver),
-                       ethernet_input);
+                      ethernet_input);
     assert(netif);
     netif_set_up(netif);
     netif_set_default(netif);
@@ -159,9 +170,10 @@ void pre_init(void) {
 /* Provided by the Ethdriver template */
 seL4_CPtr ethdriver_notification(void);
 
-int run() {
+int run()
+{
     int UNUSED error;
-    while(1) {
+    while (1) {
         seL4_Wait(ethdriver_notification(), NULL);
         error = lwip_lock();
         ethif_lwip_handle_irq(&_lwip_driver, 0);

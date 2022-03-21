e37cd788bb10193df298f66e76549d924472facf
Courtney3141 <55342552+Courtney3141@users.noreply.github.com>
2022-03-22 00:48:37 +1100

    Created the virtio_blk files I believed were necessary.

---
+++ libs/libvirtio/include/virtio/virtio_blk.h
@@ -0,0 +1,25 @@
+// TODO COURTNEY: Make this blk specific!! I believe this is the header for the second highest level of abstraction
+
+/*
+ * Copyright 2019, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <sel4vmmplatsupport/drivers/virtio_net.h>
+
+/* Maximum transmission unit for Ethernet interface */
+#define ETHERNET_HEADER_SIZE 14
+#define MAX_MTU_DATA 1500
+#define MAX_MTU MAX_MTU_DATA + ETHERNET_HEADER_SIZE
+
+typedef struct virtio_net_callbacks {
+    int (*tx_callback)(char *data, size_t length, virtio_net_t *virtio_net);
+    void (*irq_callback)(int irq, virtio_net_t *virtio_net);
+    void (*get_mac_addr_callback)(uint8_t *mac);
+} virtio_net_callbacks_t;
+
+int virtio_net_rx(char *data, size_t length, virtio_net_t *virtio_net);
+
+virtio_net_t *virtio_net_init(vm_t *vm, virtio_net_callbacks_t *callbacks,
+                              vmm_pci_space_t *pci, vmm_io_port_list_t *io_ports);

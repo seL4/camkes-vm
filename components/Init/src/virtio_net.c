4fe651cbd8a8af0276d50ab855fd04b9348e330c
Andy Bui <andy.bui@unsw.edu.au>
2022-08-15 05:21:59 +0000

    Separate virtio headers into their own lib
    Signed-off-by: Andy Bui <andy.bui@unsw.edu.au>

--- components/Init/src/virtio_net.c
+++ components/Init/src/virtio_net.c
@@ -19,9 +19,9 @@
 #include <camkes.h>
 #include <camkes/dataport.h>
 
-#include <ethdrivers/virtio/virtio_pci.h>
-#include <ethdrivers/virtio/virtio_net.h>
-#include <ethdrivers/virtio/virtio_ring.h>
+#include <virtio/virtio_pci.h>
+#include <virtio/virtio_net.h>
+#include <virtio/virtio_ring.h>
 
 #include <sel4vm/guest_vm.h>
 #include <sel4vm/guest_memory.h>

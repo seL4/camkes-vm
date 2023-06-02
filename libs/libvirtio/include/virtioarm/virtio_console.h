a4ad4da62a374f282aef4abde2a6ac19ac032726
Axel Heider <axel.heider@hensoldt.net>
2023-06-02 13:36:34 +0200

    libvirtio: remove unused include
    The APIs from this file do not require this include
    
    Signed-off-by: Axel Heider <axel.heider@hensoldt.net>

--- libs/libvirtio/include/virtioarm/virtio_console.h
+++ libs/libvirtio/include/virtioarm/virtio_console.h
@@ -7,7 +7,6 @@
 #pragma once
 
 #include <sel4vmmplatsupport/drivers/virtio_con.h>
-#include <virtqueue.h>
 
 virtio_con_t *virtio_console_init(vm_t *vm, console_putchar_fn_t putchar,
                                   vmm_pci_space_t *pci, vmm_io_port_list_t *io_ports);
